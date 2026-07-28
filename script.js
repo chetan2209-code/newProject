6cbbc1ea9c74c81f744dd9d197ccbe9d96b811c3
chetanRana <chetanr2207@gmail.com>
2026-07-28 21:45:44 +0530

    Button style updated

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-
-// const button = document.getElementById("likeBtn");
-// const likeCount = document.getElementById("likeCount");
-
-
-// let count = 0;
-// let liked = false;
-
-// button.addEventListener("click", function() {
-
-//     if (liked === false) {
-//         count = count + 1;
-//         button.classList.add("liked"); 
-//         liked = true;
-//     } else {
-//         count = count - 1;
-//         button.classList.remove("liked");
-//         liked = false;
-//     }
-//     likeCount.innerText = count;
-// });
-
 // DOM Elements ko select karna
 const likeBtn = document.getElementById("likeBtn");
 const dislikeBtn = document.getElementById("dislikeBtn");
